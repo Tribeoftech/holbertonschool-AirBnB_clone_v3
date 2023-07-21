38b0fe454fa65a30f04cd9e33f0b5467d1b16752
Nomad-Rob <rfarley89@gmail.com>
2023-07-21 10:13:04 -0500

    Wrote some helpful comments

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""script to create routes"""
-
+"""Script to create routes for API endpoints"""
 
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
-
+# Route to get the status of the API
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """Returns a JSON status"""
+    """Returns a JSON status indicating that the API is running"""
     return jsonify({"status": "OK"})
 
-
+# Route to get the count of all objects in the database
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
-    """returns the count of all objects"""
+    """Returns the count of all objects in the database"""
+    # Create a dictionary to store the counts of different objects
     stats_dict = {
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
@@ -24,4 +24,5 @@ def stats():
         "states": storage.count("State"),
         "users": storage.count("User")
     }
+    # Return the dictionary as a JSON response
     return jsonify(stats_dict)
