be48688dfc7a31854f54b29ed917b8f2638a39fe
Nomad-Rob <rfarley89@gmail.com>
2023-07-21 10:16:48 -0500

    Wrote helpful comments and pep8 changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,11 +5,13 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 # Route to get the status of the API
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns a JSON status indicating that the API is running"""
     return jsonify({"status": "OK"})
+
 
 # Route to get the count of all objects in the database
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
